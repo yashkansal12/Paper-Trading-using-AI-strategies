663e627439ade9ddf11c35e544dd35d6c1a1e38a
yash kansal <yashkansal1611@gmail.com>
2026-08-04 16:43:47 +0530

    Update context_processors.py

--- dashboard/context_processors.py
+++ dashboard/context_processors.py
@@ -1,5 +1,22 @@
 from market_data.services import get_market_indices
 
-
 def market_data(request):
-    return {"market": get_market_indices()}
+    try:
+        return {
+            "market": get_market_indices()
+        }
+    except Exception as e:
+        print("Context Processor Error:", e)
+        return {
+            "market": {}
+        }
+
+
+
+
+
+# from market_data.services import get_market_indices
+
+
+# def market_data(request):
+#     return {"market": get_market_indices()}
